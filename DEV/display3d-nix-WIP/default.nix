9f86465f4d7f61b3cf5ed926b0a354dc57c47c51
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2025-01-12 18:39:57 +0100

    display 3d dev

---
+++ DEV/display3d-nix-WIP/default.nix
@@ -0,0 +1,29 @@
+{
+  rustPlatform,
+  fetchFromGitHub,
+  lib,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "display3d";
+  version = "0.1.17";
+  
+  src = fetchFromGitHub {
+    owner = "renpenguin";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-CqbYFz78hjEhqzHoziWDvBPMmhAjx2Sqx56PSM81Pd4="; # richtiger hash prop
+  };
+
+  cargoHash = "";
+
+  doInstallCheck = true;
+
+  meta = with lib; {
+    description = "CLI for rendering and animating 3D objects";
+    mainProgram = "display3D";
+    homepage = "https://github.com/renpenguin/display3d";
+    license = licenses.mit;
+    maintainers = [ quentin ];
+  }; 
+}
