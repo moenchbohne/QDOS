3af04abce68ff826524a2e0f8c046e6ea10a2df4
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2025-02-10 10:10:05 +0100

    inviska

--- DEV/inviska-nix-WIP/default.nix
+++ DEV/inviska-nix-WIP/default.nix
@@ -5,26 +5,21 @@
 }:
 
 let
-  version = "1.4.0";
-  pname = "quba";
+  version = "11.0";
+  pname = "Inviska";
 
   src = fetchurl {
-    url = "https://www.videohelp.com/download/Inviska_MKV_Extract-11.0-x86_64.AppImage";
-    hash = "sha256-EsTF7W1np5qbQQh3pdqsFe32olvGK3AowGWjqHPEfoM=";
+    url = "https://www.videohelp.com/download/${pname}_MKV_Extract-${version}-x86_64.AppImage";
+    hash = "sha256-nv+ouKyS1Gd9tf1j7Cz5F+AFmqTu5Ql543yqVylDC/I=";
   };
 
-  appimageContents = appimageTools.extractType1 { inherit name src; };
+  appimageContents = appimageTools.extractType1 { inherit pname src; };
 in
 appimageTools.wrapType2 rec {
   inherit pname version src;
 
-  extraInstallCommands = ''
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace-fail 'Exec=AppRun' 'Exec=${meta.mainProgram}'
-  '';
-
   meta = {
-    description = "Viewer for electronic invoices";
+    description = "Audio extract from mkv containers";
     homepage = "https://www.videohelp.com/software/Inviska-MKV-Extract";
     downloadPage = "https://www.videohelp.com/software/Inviska-MKV-Extract";
     license = lib.licenses.gpl2Only;
