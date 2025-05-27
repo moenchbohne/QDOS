19ed9a6f75beb8b40472f007d6757df2f3af97df
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2025-05-27 10:57:21 +0200

    sorted inputs in flake

--- nixos-config/flake.nix
+++ nixos-config/flake.nix
@@ -7,21 +7,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    
     nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     musnix.url = "github:musnix/musnix"; 
+
     # stylix.url = "github:danth/stylix";
+
     zen-browser.url = "github:MarceColl/zen-browser-flake";
+
     nix-flatpak.url = "github:gmodena/nix-flatpak";
+
     nix-snapd = {
       url = "github:nix-community/nix-snapd";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nix-doom-emacs = {
       url = "github:nix-community/nix-doom-emacs";
       inputs.nixpkgs.follows = "nixpkgs";
