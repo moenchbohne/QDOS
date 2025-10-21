b7857e38a76ca68665bb41c82384c57a52050406
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2025-10-21 14:54:15 +0200

    use krusader!!!

---
+++ nixos-config/modules/apps/krusader.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, lib, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    # the holy fm
+    krusader
+
+    # krusader integrated tools
+    kdePackages.kget # DL manager 
+    krename # rename util
+    mlocate # ???
+    busybox # diff util ?
+    zip # zip archieves
+    rar # rar archieves
+
+    # qt control outside of KDE
+    kdePackages.qt6ct
+  ];
+}
