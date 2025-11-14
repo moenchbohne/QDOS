2dcc00859ff1979db3b762eb737c7e74c00409cf
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2025-11-14 14:35:36 +0100

    rm amdvlk

--- nixos-config/modules/graphics/amd.nix
+++ nixos-config/modules/graphics/amd.nix
@@ -22,10 +22,6 @@
 
   hardware.amdgpu = {
     opencl.enable = true;
-    amdvlk = {
-      enable = true;
-      support32Bit.enable = true;
-      supportExperimental.enable = true;
     };
     legacySupport.enable = true;
   };
