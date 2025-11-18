5f2c93c1e6a5e1d7a61edafdb121d16b3548e719
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2025-11-18 12:17:44 +0100

    added speedtest

--- nixos-config/modules/uni.nix
+++ nixos-config/modules/uni.nix
@@ -3,10 +3,19 @@
 {
   environment.systemPackages = with pkgs; [
     qalculate-qt # calculator
+<<<<<<< HEAD
+    # ===== wifi =====
+    geteduroam 
+    speedtest
+=======
     geteduroam # wifi
     # ----- c-Packages -----
     gcc
     gnumake
     stdenv
+<<<<<<< Updated upstream
+=======
+>>>>>>> 8f9c6a6c89ddbbff249e5b5a300816730d3c5bf3
+>>>>>>> Stashed changes
   ];
 }
