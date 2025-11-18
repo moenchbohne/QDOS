116b887287ac9a3109a38ac56cbc479038a21776
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2025-11-18 12:23:00 +0100

    uni

--- nixos-config/modules/uni.nix
+++ nixos-config/modules/uni.nix
@@ -15,6 +15,10 @@
     stdenv
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+>>>>>>> 8f9c6a6c89ddbbff249e5b5a300816730d3c5bf3
+>>>>>>> Stashed changes
 =======
 >>>>>>> 8f9c6a6c89ddbbff249e5b5a300816730d3c5bf3
 >>>>>>> Stashed changes
