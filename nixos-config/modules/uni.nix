a2408dce950a5a281040bb1115328921a60c953b
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2025-11-18 12:30:11 +0100

    msking things right

--- nixos-config/modules/uni.nix
+++ nixos-config/modules/uni.nix
@@ -3,27 +3,12 @@
 {
   environment.systemPackages = with pkgs; [
     qalculate-qt # calculator
-<<<<<<< HEAD
     # ===== wifi =====
     geteduroam 
     speedtest
-=======
-    geteduroam # wifi
     # ----- c-Packages -----
     gcc
     gnumake
     stdenv
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
->>>>>>> 8f9c6a6c89ddbbff249e5b5a300816730d3c5bf3
->>>>>>> Stashed changes
-=======
->>>>>>> 8f9c6a6c89ddbbff249e5b5a300816730d3c5bf3
->>>>>>> Stashed changes
-=======
->>>>>>> 8f9c6a6c89ddbbff249e5b5a300816730d3c5bf3
->>>>>>> Stashed changes
   ];
 }
