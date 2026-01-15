4ef13d132b43c5706ffc480951fc54a01da5065d
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2026-01-15 11:28:01 +0100

    man fuck nushell honestly

--- nixos-config/flake.nix
+++ nixos-config/flake.nix
@@ -8,7 +8,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     
-    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-24.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
