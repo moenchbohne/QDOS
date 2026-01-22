105401fe3d70d692aad4981201ee0ae08de96e78
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2026-01-22 10:47:27 +0100

    doing flameshot

--- nixos-config/modules/user/flameshot/default.nix
+++ nixos-config/modules/user/flameshot/default.nix
@@ -9,6 +9,29 @@
 
 
   services.flameshot.settings = {
-    
+      General = {
+              
+        # savePath = "/home/user/Screenshots";
+        
+        disabledTrayIcon = true;
+        showStartupLaunchMessage = false;
+
+        saveAsFileExtension = ".png";
+
+        showDesktopNotification = true;
+        showAbortNotification = false;
+
+        # Whether to show the info panel in the center in GUI mode
+        showHelp = true;
+        showSidePanelButton = true;
+
+        # wayland compat
+        useGrimAdapter = true;
+        disabledGrimWarning = true;
+      };
   };
+
+  home.packages = with pkgs; [
+    grim
+  ];
 }
