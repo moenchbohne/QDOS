6ea017f0778b05d7e4bcb911780809e4630e6441
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2026-01-25 21:32:51 +0100

    more module

--- nixos-config/modules/system/boot/default.nix
+++ nixos-config/modules/system/boot/default.nix
@@ -1,6 +1,10 @@
   { config, lib, pkgs, ... }:
   
   {
+    imports = [
+      ./plymouth.nix
+    ];
+
     boot.loader.grub.enable = true;
     boot.loader.grub.device = "/dev/nvme0n1";
     boot.loader.grub.useOSProber = true;
