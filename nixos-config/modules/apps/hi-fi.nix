a6d3d33705b19b0ec24b129026eddbcb0a4daf88
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2026-01-31 19:18:51 +0100

    i think we doing feishin

--- nixos-config/modules/apps/hi-fi.nix
+++ nixos-config/modules/apps/hi-fi.nix
@@ -4,7 +4,11 @@
   environment.systemPackages = with pkgs; [
     # still in the POC/WIP/tryout phase
     strawberry
+
+    # together for bit perfect
     feishin
+    mpv
+
     aonsoku
     # ampcast package urself
     supersonic-wayland
