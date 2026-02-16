72c2b4165686cec9187d756062e9b81d264ca495
quentin <beckercelle@gmail.com>
2026-02-16 15:10:20 +0100

    ghostty config

--- nixos-config/modules/user/terminal/default.nix
+++ nixos-config/modules/user/terminal/default.nix
@@ -12,30 +12,30 @@
       ### Font & Appearance ###
       font-family = "JetBrains Mono";
       font-size = 12;
-      background-opacity = "0.95";
-      background-blur-radius = "20";
+      background-opacity = 0.95;
+      background-blur-radius = 20;
 
       ### Window & Tabs ###
-      window-tabs = true;
       window-padding-x = 4;
       window-padding-y = 4;
-      window-save-state = true;
+      # window-save-state = true;
       confirm-close-surface = false;
 
       ### Mouse & Selection ###
       copy-on-select = true;
-      mouse-middle-paste = true;
-      mouse-right-paste = true;
+      # mouse-middle-paste = true;
+      # mouse-right-paste = true;
 
       ### Keybinds ###
       keybind = [
         "ctrl+shift+t=new_tab"
         "ctrl+shift+w=close_surface"
         "ctrl+tab=next_tab"
-        "ctrl+shift+right=move_tab_right"
-        "ctrl+shift+left=move_tab_left"
+        "ctrl+shift+right=move_tab:1"
+        "ctrl+shift+left=move_tab:-1"
         "ctrl+shift+c=copy_to_clipboard"
         "ctrl+shift+v=paste_from_clipboard"
+        "ctrl+shift+r=reload_config"
         "ctrl+plus=increase_font_size"
         "ctrl+minus=decrease_font_size"
         "ctrl+0=reset_font_size"
