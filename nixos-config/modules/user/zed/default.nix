30b2052d6013dba9c6c2729d31eb838261c0b057
moenchbohne <beckercelle@gmail.com>
2026-03-18 11:51:40 +0100

    started zed config in HM

--- nixos-config/modules/user/zed/default.nix
+++ nixos-config/modules/user/zed/default.nix
@@ -3,5 +3,34 @@
 {
   programs.zed-editor = {
     enable = true;
+
+    extensions = [
+      "nix"
+    ];
+
+    userSettings = {
+      features = {
+        copilot = false;
+      };
+      telemetry = {
+        metrics = false;
+      };
+      lsp = {
+        nil = {
+          binary = {
+            path = {
+            /run/current-system/sw/bin/nil
+            };
+          };
+        };
+        nixd = {
+          binary = {
+            path = {
+            /run/current-system/sw/bin/nil
+            };
+          };
+        };
+      };
+    };
   };
 }
