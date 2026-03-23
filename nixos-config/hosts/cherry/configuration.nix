fab423bad656459d3de9bc58506a91e65b763e35
moenchbohne <beckercelle@gmail.com>
2026-03-23 20:01:13 +0100

    Update configuration.nix

--- nixos-config/hosts/cherry/configuration.nix
+++ nixos-config/hosts/cherry/configuration.nix
@@ -108,14 +108,6 @@
      gnome-extension-manager
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   services.openssh.enable = true;
 
   nix.settings.experimental-features = [
