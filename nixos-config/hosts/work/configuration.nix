d3cd001137855f76f7ca032af3556ab9ef2d592f
moenchbohne <beckercelle@gmail.com>
2026-04-08 11:26:45 +0200

    Update configuration.nix

--- nixos-config/hosts/work/configuration.nix
+++ nixos-config/hosts/work/configuration.nix
@@ -30,7 +30,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_535;;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
   };
 
   hardware.graphics = {
