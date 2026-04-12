cda4db8f054de22b697db089678e414786dfa398
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2026-04-12 12:29:46 +0200

    fix the module

--- nixos-config/modules/system/home_manager/default.nix
+++ nixos-config/modules/system/home_manager/default.nix
@@ -1,15 +1,24 @@
-{ ... }:
+{ inputs, pkgs-stable, ... }:
 
 {
-    home-manager.nixosModules.home-manager
-  {
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.users.quentin = import ./home/users/quentin/home.nix;
-    home-manager.backupFileExtension = "bk";
-    home-manager.extraSpecialArgs = {
-      inherit inputs;
-      inherit pkgs-stable;
+  # 1. Import the Home Manager NixOS module
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  # 2. Define the Home Manager configuration
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "backup";
+    
+    # Pass your special args down to Home Manager
+    extraSpecialArgs = {
+      inherit inputs pkgs-stable;
     };
-  }
+
+    # NOTE: Adjust this path depending on where you save this module!
+    # If this file is in ./common/home-manager.nix, the path below should step back up.
+    users.quentin = import ../../../home/users/quentin/home.nix; 
+  };
 }
