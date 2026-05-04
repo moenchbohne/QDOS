45b2341455c08e262079acac1bf6c6bf91addef2
moenchbohne <beckercelle@gmail.com>
2026-05-04 10:40:57 +0200

    vivaldi module (yes we trying again damn)

--- nixos-config/home/users/quentin/home.nix
+++ nixos-config/home/users/quentin/home.nix
@@ -20,6 +20,7 @@
     ../../../modules/user/thunderbird/default.nix
     ../../../modules/user/terminal/default.nix
     ../../../modules/user/zed/default.nix
+    ../../../modules/user/vivaldi/default.nix
   ];
 
   programs.cava.enable = true;
