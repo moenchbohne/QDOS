7391989de1ec470126a7667487749392ec3853a4
moenchbohne <122054278+moenchbohne@users.noreply.github.com>
2026-05-20 10:22:01 +0200

    added emacs config

--- nixos-config/modules/user/emacs/default.nix
+++ nixos-config/modules/user/emacs/default.nix
@@ -5,11 +5,13 @@
     package = pkgs.emacs-pgtk;
   };
 
+  # symlink the config
+  xdg.configFile."emacs/init.el".source = ./init.el;
+
   # demon emacs
   services.emacs = {
     enable = true;
     defaultEditor = true;
-    
   };
 
   home.Packages = with pkgs; [
